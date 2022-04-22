332d8d993438bc06e3136f524461fdfbfd1cec96
D. Brumazzi <brumazzi_daniel@yahoo.com.br>
2022-04-22 12:08:01 -0300

    build final code to release

--- typescript/invalid-links.ts
+++
@@ -1,8 +0,0 @@
-// disable anchor without reference
-function invalidBlankAnchor(){
-  let anchors:NodeListOf<Element> = document.querySelectorAll("a[href=''], a[href='#']")
-  let i:number = 0;
-  for(i=0; i<anchors.length; i+=1){
-    anchors[i].addEventListener("click", (e)=>{e.preventDefault()})
-  }
-}
